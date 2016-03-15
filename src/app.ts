a73818cc0f8cfe16232ab0044e0ee074d0b5957d
Ciaran O'Neill <oneillci@gmail.com>
2016-03-15 16:28:48 +0000

    simplified bug repro

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,57 @@
-import {Router, RouterConfiguration} from 'aurelia-router'
+import {inject, TemplatingEngine} from 'aurelia-framework'
 
 declare var kendo: any;
+declare var jQuery: any;
+
+@inject(Element, TemplatingEngine)
 export class App {
     message = "Hello Aurelia";
     
-    columns = [
-        {field: "name", title: "Name", template: this.getNameTemplate()},
-        {field: "age", title: "Age", template: this.getAgeTemplate()},
-        {field: "active", title: "Is Active"}
-    ];
-
     data = [
         {id: "1", name: "Ciaran", age: 36, active: true},
         {id: "2", name: "Stevie", age: 46, active: false},
     ];
- 
-    getAgeTemplate() {
-        const template = `
-<span class='enhance-grid-item' data-id='#=data.id#'>
+    
+    constructor(private element: Element, private templatingEngine: TemplatingEngine){
+    }
+    
+    attached(){
+        // 1. Finds any items with the .enhance class
+        // 2. Looks for an id in a data-id attribute
+        // 3. Finds that item by id in this.data
+        // 4. Uses the templating engine enhance to 'aurelia-ise' the element
+        var self = this;
+        var items = (<any>jQuery(this.element)).find(".enhance:not([au-target])");
+
+        if (items.length) {
+            items.each((index, element) => {
+                const id = (<any>$(element)).attr("data-id");
+                if (id) {
+                    const item = self.data.filter(x => x.id === id)[0];
+                    if (item) {
+                        const view = this.templatingEngine.enhance({ element: element, bindingContext: item });
+                        view.attached();
+                    }
+                }
+            });
+        }
+    }
+    
+    getTemplate(id){
+        return `
+<div class='enhance' data-id='${id}'>
+   <input type='text' value.bind='name' />
+</div>`;
+    }
+    
+    getClickToEditTemplate(id){
+        return `
+<span class='enhance' data-id='${id}'>
     <click-to-edit display-value='click to edit'>
         <template replace-part='content-template'>
             <input type='text' value.bind='age' />
         </template>
     </click-to-edit>
-</span>
-`;
-        return kendo.template(template);
-    }
-  
-    getNameTemplate() {
-        const template = `
-<span class='enhance-grid-item' data-id='#=data.id#'>
-    <input type='text' value.bind='name' />
-</span>
-`;
-        return kendo.template(template);
+</span>`;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,7 @@ export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-    .globalResources(["./dist/grid-view"])
     .globalResources(["./dist/click-to-edit"]);
-
-  //Uncomment the line below to enable animation.
-  //aurelia.use.plugin('aurelia-animator-css');
-
-  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  //aurelia.use.plugin('aurelia-html-import-template-loader')
 
   aurelia.start().then(() => aurelia.setRoot());
 }
